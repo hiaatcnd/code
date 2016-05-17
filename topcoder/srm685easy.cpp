273b4a92c2ea6fba7f05454d0db33647cd301e57
Xin <immortal1996@163.com>
2016-05-17 20:19:20 +0800

    Create srm685easy.cpp

---
+++ topcoder/srm685easy.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mkp make_pair
+#define fi first
+#define se second
+#define FOR(i, l, r) for(int i = l; i <= r; i++)
+#define ROF(i, r, l) for(int i = r; i >= l; i--)
+#define all(a) a.begin(), a.end()
+
+class MultiplicationTable2{
+public:
+	int f[60][60];
+	vector<int> dfs(vector<int> g){
+		sort(all(g));
+		for(auto u : g)
+			for(auto v : g){
+				if(*lower_bound(all(g), f[u][v]) != f[u][v]){
+					g.pb(f[u][v]);
+					return dfs(g);
+				}
+			}
+		return g;
+	}
+	int minimalGoodSet(vector<int> table) {
+		int n = sqrt(table.size() + 0.1);
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < n; j++)
+				f[i][j] = table[i * n + j];
+		int ans = n;
+		for(int i = 0; i < n; i++){
+			vector<int> g;
+			g.pb(i);
+			auto t = dfs(g);
+			ans = min(ans, (int)t.size());
+		}
+		return ans;
+	}
+};
