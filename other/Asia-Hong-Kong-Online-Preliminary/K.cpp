5dedd47bcce58face64ba78c267ab5511c2480a0
Xin <immortal1996@163.com>
2016-09-15 00:59:10 +0800

    Create K.cpp

---
+++ other/Asia-Hong-Kong-Online-Preliminary/K.cpp
@@ -0,0 +1,21 @@
+//看看会不会爆int!
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mkp make_pair
+#define fi first
+#define se second
+#define FOR(i, l, r) for(int i = l; i <= r; i++)
+#define ROF(i, r, l) for(int i = r; i >= l; i--)
+#define all(a) a.begin(), a.end()
+inline int ckmax(int &a, int b) { return a < b ? a = b, 1 : 0; }
+inline int ckmin(int &a, int b) { return a > b ? a = b, 1 : 0; }
+#define clean(a) memset(a, 0, sizeof(a))
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	printf("%s\n", n & 1 ? "Alice" : "Bob");
+  return 0;
+}
